0ac3ca9ef1d3c3b47b1fc1dea120657edaa7951b
Lemi Dinku <lemidinq@gmail.com>
2024-05-31 22:48:51 +0300

    chore: Refactor profile page widget and update auth provider usage

--- frontend/restaurant/lib/presentation/profile_page.dart
+++ frontend/restaurant/lib/presentation/profile_page.dart
@@ -1,69 +1,70 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:restaurant/application/auth/auth_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:restaurant/application/auth/auth_provider.dart';
 
-// class ProfilePage extends StatefulWidget {
-//   @override
-//   State<ProfilePage> createState() => _ProfilePageState();
-// }
+class ProfilePage extends ConsumerStatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
 
-// class _ProfilePageState extends State<ProfilePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocConsumer<AuthBloc, AuthState>(
-//       listener: (context, state) {
-//         // TODO: implement listener
-//       },
-//       builder: (context, state) {
-//         switch (state.runtimeType) {
-//           case AuthAuthenticated:
-//             final successState = state as AuthAuthenticated;
-//             return Scaffold(
-//               appBar: AppBar(
-//                 title: Text('Profile'),
-//               ),
-//               body: Padding(
-//                 padding: EdgeInsets.all(16.0),
-//                 child: Column(
-//                   crossAxisAlignment: CrossAxisAlignment.center,
-//                   children: [
-//                     CircleAvatar(
-//                       radius: 80,
-//                       backgroundImage: NetworkImage(
-//                           'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_640.png'), // Use the user's photo URL here
-//                     ),
-//                     SizedBox(height: 20),
-//                     Text(
-//                       successState
-//                           .user.username, // Display the user's name here
-//                       style:
-//                           TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-//                     ),
-//                     SizedBox(height: 20),
-//                     Card(
-//                       child: ListTile(
-//                         leading: Icon(Icons.shopping_basket),
-//                         title: Text('Order'),
-//                         subtitle: Text('Explore Our Restaurant'),
-//                       ),
-//                     ),
-//                     Card(
-//                       child: ListTile(
-//                         leading: Icon(Icons.phone),
-//                         title: Text('Phone'),
-//                         subtitle: Text(successState.user.phone),
-//                       ),
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//             );
-//           default:
-//             return Center(
-//               child: Text('The Token is Expired '),
-//             );
-//         }
-//       },
-//     );
-//   }
-// }
+class _ProfilePageState extends ConsumerState<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    final state = ref.watch(authNotifierProvider);
+
+    if (state is AuthLoading) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    } else if (state is AuthAuthenticated) {
+      final user = state.user;
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Profile'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const CircleAvatar(
+                radius: 80,
+                backgroundImage: NetworkImage(
+                    'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_640.png'),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                user.username,
+                style:
+                    const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              const Card(
+                child: ListTile(
+                  leading: Icon(Icons.shopping_basket),
+                  title: Text('Order'),
+                  subtitle: Text('Explore Our Restaurant'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: const Icon(Icons.phone),
+                  title: const Text('Phone'),
+                  subtitle: Text(user.phone),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    } else if (state is AuthUnauthenticated) {
+      return const Center(
+        child: Text('The Token is Expired '),
+      );
+    } else {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+  }
+}
