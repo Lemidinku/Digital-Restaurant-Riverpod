6f6b15320ffac05cc96d3c988de5517c496e3ad5
GedionAfework <gedionmekbeb2@gmail.com>
2024-05-31 19:37:13 +0300

    edited auth

--- frontend/restaurant/lib/application/auth/auth_provider.dart
+++ frontend/restaurant/lib/application/auth/auth_provider.dart
@@ -8,12 +8,10 @@ part 'auth_state.dart';
 part 'auth_notifier.dart';
 part 'auth_event.dart';
 
-// Define a provider for the AuthRepository
 final authRepositoryProvider = Provider<AuthRepository>((ref) {
   return AuthRepository(baseUrl: BASE_URL);
 });
 
-// Define a provider for the AuthNotifier
 final authNotifierProvider =
     StateNotifierProvider<AuthNotifier, AuthState>((ref) {
   final authRepository = ref.watch(authRepositoryProvider);
