38c974aefee4abdc1e969708f32bac7cb2b1e4de
Eba Adisu <ebaadisu2@gmail.com>
2024-05-31 22:11:17 +0300

    Add integration tests for authentication and meal functionalities

---
+++ frontend/restaurant/test_driver/integration_test.dart
@@ -0,0 +1,6 @@
+import 'package:integration_test/integration_test_driver.dart';
+
+Future<void> main() => integrationDriver();
+
+// flutter drive --driver=test_driver/integration_test.dart --target=integration_test/auth_test.dart
+// 
